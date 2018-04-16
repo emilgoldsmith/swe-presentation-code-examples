ec17e5085e45e42f3589a470e2044f16ca0eeaea
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2018-04-16 11:17:32 +0400

    Update it

--- race-conditions.js
+++ race-conditions.js
@@ -2,12 +2,14 @@ let isBusy = false;
 
 setTimeout(() => {
   isBusy = true;
+  console.log("critical 1");
   // Do something critical here
   isBusy = false;
-});
+}, 0);
 
 if (!isBusy) {
   isBusy = true;
+  console.log("critical 2");
   // Do something else very critical
   isBusy = false;
 }
